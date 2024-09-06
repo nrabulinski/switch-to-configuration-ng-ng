510dbb611bfbaef85aa52e6352b66af36cd95699
Nikodem Rabuliński <nikodem@rabulinski.com>
2024-09-06 20:49:57 +0200

    Make build.rs more idiomatic

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
-use std::io::Write;
+use std::{
+    env::var,
+    fs::write,
+    path::{Path, PathBuf},
+};
 
-fn code_for_dbus_xml(xml: impl AsRef<std::path::Path>) -> String {
+fn code_for_dbus_xml(xml: impl AsRef<Path>) -> String {
     dbus_codegen::generate(
         &std::fs::read_to_string(xml).unwrap(),
         &dbus_codegen::GenOpts {
@@ -13,18 +17,16 @@ fn code_for_dbus_xml(xml: impl AsRef<std::path::Path>) -> String {
 }
 
 fn main() {
-    let systemd_dbus_interface_dir = std::env::var("SYSTEMD_DBUS_INTERFACE_DIR").unwrap();
-    let systemd_dbus_interface_dir = std::path::Path::new(systemd_dbus_interface_dir.as_str());
+    let systemd_dbus_interface_dir = var("SYSTEMD_DBUS_INTERFACE_DIR").unwrap();
+    let systemd_dbus_interface_dir = Path::new(systemd_dbus_interface_dir.as_str());
 
-    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(var("OUT_DIR").unwrap());
 
     let systemd_manager_code =
         code_for_dbus_xml(systemd_dbus_interface_dir.join("org.freedesktop.systemd1.Manager.xml"));
-    let mut file = std::fs::File::create(out_path.join("systemd_manager.rs")).unwrap();
-    file.write_all(systemd_manager_code.as_bytes()).unwrap();
+    write(out_path.join("systemd_manager.rs"), systemd_manager_code).unwrap();
 
     let logind_manager_code =
         code_for_dbus_xml(systemd_dbus_interface_dir.join("org.freedesktop.login1.Manager.xml"));
-    let mut file = std::fs::File::create(out_path.join("logind_manager.rs")).unwrap();
-    file.write_all(logind_manager_code.as_bytes()).unwrap();
+    write(out_path.join("logind_manager.rs"), logind_manager_code).unwrap();
 }
